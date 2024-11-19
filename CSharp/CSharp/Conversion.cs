66518c0e7e53f7ab4e1f9435c94e4a8efaead89a
Krarwi <glimmergo0se@gmail.com>
2024-11-19 10:23:11 +0200

    upload

---
+++ CSharp/CSharp/Conversion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    internal class Conversion
+    {
+        static void Main(string[] args)
+        {
+            string numbertext = "2";
+            int number = int.Parse(numbertext);
+            Console.WriteLine(number);
+        }
+    }
+}
