32edf4bb5b5bc9bda9cf757a21de91edc18734d0
Krarwi <glimmergo0se@gmail.com>
2024-11-19 09:27:53 +0200

    upload

---
+++ Input output.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    internal class Input_output
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Username: ");
+            string username = Console.ReadLine();
+            Console.WriteLine("Welcome " + username + "!");
+
+        }
+    }
+}
