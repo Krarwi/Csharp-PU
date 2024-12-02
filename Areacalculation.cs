cdd12c5b2946b9cd776744a727b0fbb7be2be89b
Krarwi <glimmergo0se@gmail.com>
2024-12-02 13:35:09 +0200

    Add files via upload

---
+++ Areacalculation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp
+{
+    internal class Areacalculation
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the width of your triangle");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input the length of your triangle");
+            int length = Convert.ToInt32(Console.ReadLine());
+
+            int answer = formula(width, length);
+
+            Console.WriteLine($"Calculated area of the triangle: {answer}");
+
+        }
+        static int formula(int width, int length)
+        {
+            return width * length / 2;
+        }
+    }
+}
---
+++ Integerarraytask.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp
+{
+    internal class Integerarraytask
+    {
+        static void Main(string[] args)
+        {
+            int[] number = { 0, 1, 2, 3, 4, 5 };
+
+            int sumofnumbersv = sumofnumbers(number);
+
+            if (sumofnumbersv > -1)
+            {
+                Console.WriteLine($"The total is {sumofnumbersv}");
+                Console.WriteLine(number.Length);
+            }
+            else
+            {
+                Console.WriteLine("Cannot add up an empty array");
+            }
+        }
+        static int sumofnumbers(int[] integer)
+        {
+            return integer.Sum();
+        }
+    }
+}
